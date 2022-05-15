6851c5c24888b16c2cab3f37f1840af57ea16ae1
Mehmet Nuri ÖZTÜRK <mnozturk@outlook.com.tr>
2022-05-15 13:11:31 +0300

    Odev Cozumu

---
+++ 01_Odev_Cozum.py
@@ -0,0 +1,9 @@
+# ÖDEV .1 # SORU Klavyeden girilen 2 sayıdan 1. nin büyük olup olmadığını ekrana yazdıran kod..
+
+birinci_sayi =  int(input("Birinci sayi:"))
+ikinci_sayi =  int(input("İkinci sayi:"))
+
+kontrol = birinci_sayi > ikinci_sayi
+
+print(kontrol)
+print(birinci_sayi>ikinci_sayi)
